653d6a900a4d5fbccbd75e6842e1810b75574755
Sandeep Mistry <s.mistry@arduino.cc>
2020-02-06 16:19:13 -0500

    Add CameraCaptureRawBytes example with acccompanying Processing sketch

---
+++ examples/CameraCaptureRawBytes/CameraCaptureRawBytes.ino
@@ -0,0 +1,56 @@
+/*
+  OV767X - Camera Capture Raw Bytes
+
+  This sketch reads a frame from the OmniVision OV7670 camera
+  and writes the bytes to the Serial port. Use the Procesing
+  sketch in the extras folder to visualize the camera output.
+
+  Circuit:
+    - Arduino Nano 33 BLE board
+    - OV7670 camera module:
+      - 3.3 connected to 3.3
+      - GND connected GND
+      - SIOC connected to A5
+      - SIOD connected to A4
+      - VSYNC connected to 8
+      - HREF connected to 10
+      - PCLK connected to 12
+      - XCLK connected to 9
+      - D7 connected to 7
+      - D6 connected to 6
+      - D5 connected to 5
+      - D4 connected to 4
+      - D3 connected to 3
+      - D2 connected to 2
+      - D1 connected to 1 / RX
+      - D0 connected to 0 / TX
+
+  This example code is in the public domain.
+*/
+
+#include <Arduino_OV767X.h>
+
+int bytesPerFrame;
+
+byte data[320 * 240 * 2]; // QVGA: 320x240 X 2 bytes per pixel (RGB565)
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  if (!Camera.begin(QVGA, RGB565, 1)) {
+    Serial.println("Failed to initialize camera!");
+    while (1);
+  }
+
+  bytesPerFrame = Camera.width() * Camera.height() * Camera.bytesPerPixel();
+
+  // Optionally, enable the test pattern for testing
+  // Camera.testPattern();
+}
+
+void loop() {
+  Camera.readFrame(data);
+
+  Serial.write(data, bytesPerFrame);
+}
